67766797a06bc39cf0827c1da69bf5c80a07fb36
frank jing <frank-jing@qq.com>
2020-11-21 17:30:57 +0800

    OID Augmentation Run OK.

---
+++ prepare_oid.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# __author__="Frank Jing"
+
+
+import copy
+import os
+import pickle
+import platform
+from typing import *
+
+import numpy as np
+import pandas as pd
+import torch
+from cv2 import cv2
+from detectron2.data import DatasetCatalog, MetadataCatalog
+from detectron2.data import detection_utils
+from detectron2.data import transforms as T
+from detectron2.structures import BoxMode, Instances, Boxes, BitMasks
+from tqdm import tqdm
+
+RUN_ON = 'local' if platform.node() == 'frank-note' else 'google-cloud'
+
+if RUN_ON == 'local':
+    DETECTRON2_DIR = '/data/venv-pytorch/detectron2'
+    OID_DIR        = '/data/venv-tensorflow2/open-images-dataset'
+else:
+    DETECTRON2_DIR = '/home/tyushang_gmail_com/jupyter/detectron2'
+    OID_DIR        = '/home/jupyter/datasets/oid'
+
+MASK_THRESHOLD = 127
+
+
+def get_paths(oid_root, tvt):
+    """
+    @param oid_root:
+    @param tvt: 'train', 'validation' or 'test'
+    Dataset Dir Tree:
+    ROOT:
+    |-- annotation-instance-segmentation
+    |   |-- metadata
+    |   |   |-- challenge-2019-classes-description-segmentable.csv  # class_csv for short.
+    |   |   |-- challenge-2019-label300-segmentable-hierarchy.json  # hierarchy_json for short
+    |   |-- train
+    |   |   |-- challenge-2019-train-masks
+    |   |   |   |-- challenge-2019-train-segmentation-masks.csv     # mask_csv for short.
+    |   |   |   |-- challenge-2019-train-masks-[0~f].zip
+    |   |   |-- all-masks                                           # N_MASK: 2125530
+    |   |   |-- challenge-2019-train-segmentation-bbox.csv          # bbox_csv for short.
+    |   |   |-- challenge-2019-train-segmentation-labels.csv        # label_csv for short.
+    |   |-- validation
+    |       |-- challenge-2019-validation-masks
+    |       |   |-- challenge-2019-validation-segmentation-masks.csv
+    |       |   |-- challenge-2019-validation-masks-[0~f].zip
+    |       |-- all-masks                                           # N_MASK: 23366
+    |       |-- challenge-2019-validation-segmentation-bbox.csv
+    |       |-- challenge-2019-validation-segmentation-labels.csv
+    |-- train       # N_IMAGE
+    |-- validation  # N_IMAGE
+    |-- test        # N_IMAGE
+    """
+    return {
+        'klass_csv' : os.path.join(oid_root, 'annotation-instance-segmentation/metadata/'
+                                             'challenge-2019-classes-description-segmentable.csv'),
+        'mask_csv'  : os.path.join(oid_root, f'annotation-instance-segmentation/{tvt}/'
+                                             f'challenge-2019-{tvt}-masks/challenge-2019-{tvt}-segmentation-masks.csv'),
+        'images_dir': os.path.join(oid_root, tvt),
+        'masks_dir' : os.path.join(oid_root, f'annotation-instance-segmentation/{tvt}/all-masks'),
+    }
+
+
+klass_df  = pd.read_csv(get_paths(OID_DIR, 'train')['klass_csv'], names=['MID', 'name'])\
+            .rename_axis(index='No').reset_index()
+MID_TO_NO = klass_df.set_index('MID')['No'].to_dict()
+
+
+def get_descs(mask_csv_path):
+    mask_pkl_path = os.path.splitext(mask_csv_path)[0] + '.pkl'
+    # cache dataset_descs to speed-up.
+    if os.path.exists(mask_pkl_path):
+        with open(mask_pkl_path, 'rb') as f:
+            dataset_descs = pickle.load(f)
+    else:
+        dataset_descs = create_descs_from_csv(mask_csv_path)
+        # cache dataset_descs for next usage.
+        with open(mask_pkl_path, 'wb') as f:
+            pickle.dump(dataset_descs, f)
+
+    return dataset_descs
+
+
+def create_descs_from_csv(mask_csv_path):
+    # columns: ['ImageID', 'MaskPath', 'LabelName', 'BoxID', 'BoxXMin', 'BoxXMax',
+    #           'BoxYMin', 'BoxYMax', 'PredictedIoU', 'Clicks']
+    mask_df = pd.read_csv(mask_csv_path, usecols=['ImageID', 'MaskPath', 'LabelName',
+                                                  'BoxXMin', 'BoxXMax', 'BoxYMin', 'BoxYMax'])
+    grouped = mask_df.groupby('ImageID')
+    # Detectron2 Standard Dataset Dicts:
+    # file_name:      full path to image file;
+    # height, width:  shape of image;
+    # image_id:
+    # annotations:    list[dict]: Each dict contains the following keys,
+    #                 of which bbox,bbox_mode and category_id are required:
+    #     bbox:
+    #     bbox_mode: structure.BoxMode: BoxMode.XYXY_ABS, BoxMode.XYHW_ABS
+    #     category_id: integer in [0, n_class -1]
+    #     segmentation: polygon: list[list[float]] | RLE: dict
+    #     keypoints: ...
+    #     iscrowd: ...
+    # sem_seg_file_name: used for semantic segmentation.
+    dataset_descs = []
+    for image_id, group_df in tqdm(grouped):
+        annotations = []
+        for _, row in group_df.iterrows():
+            # 'MaskPath' here actually is mask file name(e.g. 88e582a7b14e34a8_m039xj__6133896f.png)
+            mask_fname, _, mid, x1, x2, y1, y2 = \
+                row[['MaskPath', 'ImageID', 'LabelName', 'BoxXMin', 'BoxXMax', 'BoxYMin', 'BoxYMax']]
+            mask_id = os.path.splitext(mask_fname)[0]
+            annotations.append({
+                'bbox'       : [x1, y1, x2, y2],
+                'bbox_mode'  : BoxMode.XYXY_REL,
+                'mask_id'    : mask_id,
+                'category_id': MID_TO_NO[mid],
+            })
+        dataset_descs.append({
+            'image_id'   : image_id,
+            'annotations': annotations
+        })
+
+    return dataset_descs
+
+
+def make_mapper(dataset_name, augmentations: T.AugmentationList = None):
+    metadata   = MetadataCatalog.get(dataset_name)
+    images_dir = metadata.images_dir
+    masks_dir  = metadata.masks_dir
+
+    def _desc_to_example(desc: Dict):
+        # Detectron2 Model Input Format:
+        # image: Tensor[C, H, W];
+        # height, width: output height and width;
+        # instances: Instances Object to training, with the following fields:
+        #     "gt_boxes":
+        #     "gt_classes":
+        #     "gt_masks": a PolygonMasks or BitMasks object storing N masks, one for each instance.
+        desc       = copy.deepcopy(desc)  # it will be modified by code below
+        image_path = os.path.join(images_dir, f'{desc["image_id"]}.jpg')
+        # shape: [H, W, C]
+        origin_image = detection_utils.read_image(image_path, format="BGR")
+        oh, ow, oc = origin_height, origin_width, origin_channels = origin_image.shape
+        if augmentations is not None:
+            aug_input   = T.AugInput(origin_image)
+            transforms  = augmentations(aug_input)
+            auged_image = aug_input.image
+        else:
+            auged_image = origin_image
+        ah, aw, ac = auged_height, auged_width, auged_channels = auged_image.shape
+
+        target = Instances(image_size=(ah, aw))
+        if 'fill gt_boxes':
+            # shape: n_box, 4
+            boxes     = np.array([anno['bbox'] for anno in desc['annotations']])
+            boxes_abs = boxes * np.array([ow, oh, ow, oh])
+            if augmentations is not None:
+                # clip transformed bbox to image size
+                boxes_auged = transforms.apply_box(np.array(boxes_abs)).clip(min=0)
+                boxes_auged = np.minimum(boxes_auged, np.array([aw, ah, aw, ah])[np.newaxis, :])
+            else:
+                boxes_auged = boxes_abs
+            target.gt_boxes = Boxes(boxes_auged)
+        if 'fill gt_classes':
+            classes = [anno['category_id'] for anno in desc['annotations']]
+            classes = torch.tensor(classes, dtype=torch.int64)
+            target.gt_classes = classes
+        if 'fill gt_masks':
+            mask_paths = [os.path.join(masks_dir, f'{anno["mask_id"]}.png') for anno in desc['annotations']]
+            masks = np.array(list(map(
+                lambda p: cv2.resize(cv2.imread(p, flags=cv2.IMREAD_GRAYSCALE), dsize=(ow, oh)), mask_paths
+            )))
+            if augmentations is not None:
+                masks_auged = np.array(list(map(
+                    lambda x: transforms.apply_segmentation(x), masks
+                )))
+            else:
+                masks_auged = masks
+            masks_auged = masks_auged > MASK_THRESHOLD
+            masks_auged = BitMasks(
+                torch.stack([torch.from_numpy(np.ascontiguousarray(x)) for x in masks_auged])
+            )
+            target.gt_masks = masks_auged
+
+        return {
+            # expected shape: [C, H, W]
+            "image"    : torch.as_tensor(np.ascontiguousarray(auged_image.transpose(2, 0, 1))),
+            "height"   : auged_height,
+            "width"    : auged_width,
+            "instances": target,  # refer: annotations_to_instances()
+        }
+
+    return _desc_to_example
+
+
+# register dataset
+for tv in ["train", "validation"]:
+    paths = get_paths(OID_DIR, tv)
+    ds_name = "oid_" + tv
+    if ds_name in DatasetCatalog.list():
+        DatasetCatalog.remove(ds_name)
+    # register oid dataset dicts.
+    DatasetCatalog.register("oid_" + tv, lambda x=paths['mask_csv']: get_descs(x))
+    # set oid metadata.
+    MetadataCatalog.get("oid_" + tv).set(images_dir=paths['images_dir'])
+    MetadataCatalog.get("oid_" + tv).set(masks_dir=paths['masks_dir'])
+
+
+if __name__ == "__main__":
+    ...
+
--- train_oid.py
+++ train_oid.py
@@ -5,8 +5,10 @@
 
 
 import copy
+import datetime
 import logging
 import os
+import socket
 import sys
 import pickle
 import platform
@@ -33,8 +35,10 @@ from detectron2.utils.events import CommonMetricPrinter, JSONWriter, Tensorboard
 from detectron2.data import transforms as T
 from tqdm import tqdm
 
-# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+from prepare_oid import make_mapper
 
+# ____________________________ Config __________________________________________
+# os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 N_GPU  = torch.cuda.device_count()
 RUN_ON = 'local' if platform.node() == 'frank-note' else 'google-cloud'
 
@@ -46,190 +50,6 @@ else:
     OID_DIR        = '/home/jupyter/datasets/oid'
 
 MASK_THRESHOLD = 127
-
-
-def get_paths(oid_root, tvt):
-    """
-    @param oid_root:
-    @param tvt: 'train', 'validation' or 'test'
-    Dataset Dir Tree:
-    ROOT:
-    |-- annotation-instance-segmentation
-    |   |-- metadata
-    |   |   |-- challenge-2019-classes-description-segmentable.csv  # class_csv for short.
-    |   |   |-- challenge-2019-label300-segmentable-hierarchy.json  # hierarchy_json for short
-    |   |-- train
-    |   |   |-- challenge-2019-train-masks
-    |   |   |   |-- challenge-2019-train-segmentation-masks.csv     # mask_csv for short.
-    |   |   |   |-- challenge-2019-train-masks-[0~f].zip
-    |   |   |-- all-masks                                           # N_MASK: 2125530
-    |   |   |-- challenge-2019-train-segmentation-bbox.csv          # bbox_csv for short.
-    |   |   |-- challenge-2019-train-segmentation-labels.csv        # label_csv for short.
-    |   |-- validation
-    |       |-- challenge-2019-validation-masks
-    |       |   |-- challenge-2019-validation-segmentation-masks.csv
-    |       |   |-- challenge-2019-validation-masks-[0~f].zip
-    |       |-- all-masks                                           # N_MASK: 23366
-    |       |-- challenge-2019-validation-segmentation-bbox.csv
-    |       |-- challenge-2019-validation-segmentation-labels.csv
-    |-- train       # N_IMAGE
-    |-- validation  # N_IMAGE
-    |-- test        # N_IMAGE
-    """
-    return {
-        'klass_csv' : os.path.join(oid_root, 'annotation-instance-segmentation/metadata/'
-                                             'challenge-2019-classes-description-segmentable.csv'),
-        'mask_csv'  : os.path.join(oid_root, f'annotation-instance-segmentation/{tvt}/'
-                                             f'challenge-2019-{tvt}-masks/challenge-2019-{tvt}-segmentation-masks.csv'),
-        'images_dir': os.path.join(oid_root, tvt),
-        'masks_dir' : os.path.join(oid_root, f'annotation-instance-segmentation/{tvt}/all-masks'),
-    }
-
-
-klass_df  = pd.read_csv(get_paths(OID_DIR, 'train')['klass_csv'], names=['MID', 'name'])\
-            .rename_axis(index='No').reset_index()
-MID_TO_NO = klass_df.set_index('MID')['No'].to_dict()
-
-
-def get_descs(mask_csv_path):
-    mask_pkl_path = os.path.splitext(mask_csv_path)[0] + '.pkl'
-    # cache dataset_descs to speed-up.
-    if os.path.exists(mask_pkl_path):
-        with open(mask_pkl_path, 'rb') as f:
-            dataset_descs = pickle.load(f)
-    else:
-        dataset_descs = create_descs_from_csv(mask_csv_path)
-        # cache dataset_descs for next usage.
-        with open(mask_pkl_path, 'wb') as f:
-            pickle.dump(dataset_descs, f)
-
-    return dataset_descs
-
-
-def create_descs_from_csv(mask_csv_path):
-    # columns: ['ImageID', 'MaskPath', 'LabelName', 'BoxID', 'BoxXMin', 'BoxXMax',
-    #           'BoxYMin', 'BoxYMax', 'PredictedIoU', 'Clicks']
-    mask_df = pd.read_csv(mask_csv_path, usecols=['ImageID', 'MaskPath', 'LabelName',
-                                                  'BoxXMin', 'BoxXMax', 'BoxYMin', 'BoxYMax'])
-    grouped = mask_df.groupby('ImageID')
-    # Detectron2 Standard Dataset Dicts:
-    # file_name:      full path to image file;
-    # height, width:  shape of image;
-    # image_id:
-    # annotations:    list[dict]: Each dict contains the following keys,
-    #                 of which bbox,bbox_mode and category_id are required:
-    #     bbox:
-    #     bbox_mode: structure.BoxMode: BoxMode.XYXY_ABS, BoxMode.XYHW_ABS
-    #     category_id: integer in [0, n_class -1]
-    #     segmentation: polygon: list[list[float]] | RLE: dict
-    #     keypoints: ...
-    #     iscrowd: ...
-    # sem_seg_file_name: used for semantic segmentation.
-    dataset_descs = []
-    for image_id, group_df in tqdm(grouped):
-        annotations = []
-        for _, row in group_df.iterrows():
-            # 'MaskPath' here actually is mask file name(e.g. 88e582a7b14e34a8_m039xj__6133896f.png)
-            mask_fname, _, mid, x1, x2, y1, y2 = \
-                row[['MaskPath', 'ImageID', 'LabelName', 'BoxXMin', 'BoxXMax', 'BoxYMin', 'BoxYMax']]
-            mask_id = os.path.splitext(mask_fname)[0]
-            annotations.append({
-                'bbox'       : [x1, y1, x2, y2],
-                'bbox_mode'  : BoxMode.XYXY_REL,
-                'mask_id'    : mask_id,
-                'category_id': MID_TO_NO[mid],
-            })
-        dataset_descs.append({
-            'image_id'   : image_id,
-            'annotations': annotations
-        })
-
-    return dataset_descs
-
-
-def make_mapper(dataset_name, augmentations=None):
-    metadata   = MetadataCatalog.get(dataset_name)
-    images_dir = metadata.images_dir
-    masks_dir  = metadata.masks_dir
-    if augmentations is not None:
-        augmentations = T.AugmentationList(augmentations)
-
-    def _desc_to_example(desc: Dict):
-        # Detectron2 Model Input Format:
-        # image: Tensor[C, H, W];
-        # height, width: output height and width;
-        # instances: Instances Object to training, with the following fields:
-        #     "gt_boxes":
-        #     "gt_classes":
-        #     "gt_masks": a PolygonMasks or BitMasks object storing N masks, one for each instance.
-        desc       = copy.deepcopy(desc)  # it will be modified by code below
-        image_path = os.path.join(images_dir, f'{desc["image_id"]}.jpg')
-        # shape: [H, W, C]
-        origin_image = detection_utils.read_image(image_path, format="BGR")
-        oh, ow, oc = origin_height, origin_width, origin_channels = origin_image.shape
-        if augmentations is not None:
-            aug_input   = T.AugInput(origin_image)
-            transforms  = augmentations(aug_input)
-            auged_image = aug_input.image
-        else:
-            auged_image = origin_image
-        ah, aw, ac = auged_height, auged_width, auged_channels = auged_image.shape
-
-        target = Instances(image_size=(ah, aw))
-        if 'fill gt_boxes':
-            # shape: n_box, 4
-            boxes     = np.array([anno['bbox'] for anno in desc['annotations']])
-            boxes_abs = boxes * np.array([aw, ah, aw, ah])
-            if augmentations is not None:
-                # clip transformed bbox to image size
-                boxes_auged = transforms.apply_box(np.array(boxes_abs)).clip(min=0)
-                boxes_auged = np.minimum(boxes_auged, np.array([aw, ah, aw, ah])[np.newaxis, :])
-            else:
-                boxes_auged = boxes_abs
-            target.gt_boxes = Boxes(boxes_auged)
-        if 'fill gt_classes':
-            classes = [anno['category_id'] for anno in desc['annotations']]
-            classes = torch.tensor(classes, dtype=torch.int64)
-            target.gt_classes = classes
-        if 'fill gt_masks':
-            mask_paths = [os.path.join(masks_dir, f'{anno["mask_id"]}.png') for anno in desc['annotations']]
-            masks = np.array(list(map(
-                lambda p: cv2.resize(cv2.imread(p, flags=cv2.IMREAD_GRAYSCALE), dsize=(ow, oh)), mask_paths
-            )))
-            if augmentations is not None:
-                masks_auged = np.array(list(map(
-                    lambda x: transforms.apply_segmentation(x), masks
-                )))
-            else:
-                masks_auged = masks
-            masks_auged = masks_auged > MASK_THRESHOLD
-            masks_auged = BitMasks(
-                torch.stack([torch.from_numpy(np.ascontiguousarray(x)) for x in masks_auged])
-            )
-            target.gt_masks = masks_auged
-
-        return {
-            # expected shape: [C, H, W]
-            "image"    : torch.as_tensor(np.ascontiguousarray(origin_image.transpose(2, 0, 1))),
-            "height"   : auged_height,
-            "width"    : auged_width,
-            "instances": target,  # refer: annotations_to_instances()
-        }
-
-    return _desc_to_example
-
-
-# register dataset
-for tv in ["train", "validation"]:
-    paths = get_paths(OID_DIR, tv)
-    ds_name = "oid_" + tv
-    if ds_name in DatasetCatalog.list():
-        DatasetCatalog.remove(ds_name)
-    # register oid dataset dicts.
-    DatasetCatalog.register("oid_" + tv, lambda x=paths['mask_csv']: get_descs(x))
-    # set oid metadata.
-    MetadataCatalog.get("oid_" + tv).set(images_dir=paths['images_dir'])
-    MetadataCatalog.get("oid_" + tv).set(masks_dir=paths['masks_dir'])
 
 
 # def do_test(cfg, model):
@@ -249,6 +69,34 @@ for tv in ["train", "validation"]:
 #     return results
 
 DATA_LOADER = None
+CONFIG = None
+
+
+class MemStatsInDataFrame:
+    def __init__(self):
+        self.all_ms_df: pd.DataFrame = None
+        self.sn = 0
+
+    def record(self, memo=None):
+        ms = torch.cuda.memory_stats()
+        index = [f'{self.sn:03d}:{memo or "unnamed"}']
+        ms_df = pd.DataFrame([ms.values()], columns=ms.keys(), index=index)
+        if self.all_ms_df is None:
+            self.all_ms_df = ms_df
+        else:
+            self.all_ms_df = self.all_ms_df.append(ms_df)
+        self.sn += 1
+
+    def dump(self, path):
+        self.all_ms_df.to_csv(path, index_label='memo')
+
+
+# __________________ For Debug _____________________________
+tic = None
+
+mem_stats_df = MemStatsInDataFrame()
+# tcp_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# tcp_sock.connect(('localhost', 9999))
 
 
 def main(args):
@@ -261,28 +109,34 @@ def main(args):
         default_setup(
             cfg, args
         )  # if you don't like any of the default setup, write your own setup code
+        global CONFIG
+        CONFIG = cfg
 
     if N_GPU > 0:
+        # __________________ For Debug _____________________________
+        # mem_stats_df.record('Before-Build-Model')
         if 'build_model(cfg)':
             meta_arch = cfg.MODEL.META_ARCHITECTURE
             model = META_ARCH_REGISTRY.get(meta_arch)(cfg)
             model.to(torch.device(cfg.MODEL.DEVICE))
+        # __________________ For Debug _____________________________
+        # mem_stats_df.record('After-Build-Model')
 
     if 'do-train':
         if 'build_detection_train_loader':
-            # oid dataset.
-            if 'get_detection_dataset_dicts':
-                descs_train: List[Dict] = DatasetCatalog.get("oid_train")
-                descs_valid: List[Dict] = DatasetCatalog.get("oid_validation")
-            dataset = DatasetFromList(descs_train, copy=False)
-            if 'DatasetMapper':
-                augs = build_augmentation(cfg, is_train=True)
-            dataset = MapDataset(dataset, make_mapper('oid_train', augs))
-            # coco dataset.
-            # descs_train: List[Dict] = DatasetCatalog.get("coco_2017_train")
-            # dataset = DatasetFromList(descs_train, copy=False)
-            # mapper = DatasetMapper(cfg, True)
-            # dataset = MapDataset(dataset, mapper)
+            if 'coco_2017_train' in cfg.DATASETS.TRAIN:
+                descs_train: List[Dict] = DatasetCatalog.get("coco_2017_train")
+                dataset = DatasetFromList(descs_train, copy=False)
+                mapper = DatasetMapper(cfg, True)
+            else:  # Open-Image-Dataset
+                if 'get_detection_dataset_dicts':
+                    descs_train: List[Dict] = DatasetCatalog.get("oid_train")
+                    descs_valid: List[Dict] = DatasetCatalog.get("oid_validation")
+                dataset = DatasetFromList(descs_train, copy=False)
+                if 'DatasetMapper':
+                    augs = build_augmentation(cfg, is_train=True)
+                    mapper = make_mapper('oid_train', T.AugmentationList(augs))
+            dataset = MapDataset(dataset, mapper)
 
             sampler = TrainingSampler(len(dataset))
             data_loader = build_batch_data_loader(
@@ -325,12 +179,24 @@ def main(args):
             )
             logger = logging.getLogger("detectron2")
             logger.info("Starting training from iteration {}".format(start_iter))
+
+            # __________________ For Debug _____________________________
+            # mem_stats_df.record('Before-Iteration')
             with EventStorage(start_iter) as storage:
                 for data, iteration in zip(data_loader, range(start_iter, max_iter)):
                     iteration = iteration + 1
                     storage.step()
+                    # __________________ For Debug _____________________________
+                    # bat_image_shape = [x['image'].shape for x in data]
+                    # bat_n_anno = [len(x['instances']) for x in data]
+                    # print('_'*10 + f'Image Shape: {bat_image_shape}; Num Anno: {bat_n_anno}')
 
+                    # __________________ For Debug _____________________________
+                    # mem_stats_df.record('Before-Forward')
                     loss_dict = model(data)
+                    # __________________ For Debug _____________________________
+                    # mem_stats_df.record('After-Forward')
+
                     losses = sum(loss_dict.values())
                     assert torch.isfinite(losses).all(), loss_dict
 
@@ -357,7 +223,21 @@ def main(args):
                     if iteration - start_iter > 5 and (iteration % 20 == 0 or iteration == max_iter):
                         for writer in writers:
                             writer.write()
+                        # __________________ For Debug _____________________________
+                        # mem_summary = torch.cuda.memory_summary()
+                        # tcp_sock.send(mem_summary.encode('utf-8'))
+                        global tic
+                        if tic is None:
+                            tic = datetime.datetime.now()
+                        else:
+                            toc = datetime.datetime.now()
+                            print('_' * 35 + f'Time Elapsed: {(toc - tic).total_seconds()} s')
+                            tic = toc
+
                     periodic_checkpointer.step(iteration)
+
+            # __________________ For Debug _____________________________
+            # mem_stats_df.dump('./mem_stats_df.csv')
 
 
 if __name__ == "__main__":
@@ -367,9 +247,11 @@ if __name__ == "__main__":
         CLI_ARGS = [
             '--config-file', 'configs-oid/mask_rcnn_R_50_FPN_3x.yaml',
             '--num-gpus', f'{N_GPU}',
-            'SOLVER.IMS_PER_BATCH', '2', 'SOLVER.BASE_LR', '0.0025',
+            'SOLVER.IMS_PER_BATCH', '4', 'SOLVER.BASE_LR', '0.0025',
             'DATASETS.TRAIN', '("oid_train", )',
             'DATASETS.TEST', '("oid_validation", )',
+            # For Debug ____________________
+            # 'SOLVER.MAX_ITER', '20_000',
             # INPUT.FORMAT?  INPUT.MASK_FORMAT?
             # '--opts',
             # 'MODEL.WEIGHTS', './weights/model_final_f10217.pkl',
